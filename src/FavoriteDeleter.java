3e12e3f2a403d3dfab3e83392eab9a98dd0a9845
Scotty Ward <sward2011@my.fit.edu>
2016-05-18 18:23:36 -0400

    Initial Commit with project
    Eclipse Project setup. External Library twitter4j is being used.

---
+++ src/FavoriteDeleter.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Scotty Ward
+ * Twitter: @Scotty_ward9
+ * Email: sward2011@my.fit.edu
+ *
+ */
+public class FavoriteDeleter {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+}
