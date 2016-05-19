dd415977cdd86d68155c0a13905b9a24942437c0
Scotty Ward <sward2011@my.fit.edu>
2016-05-18 20:35:34 -0400

    Added some Ratelimit checks
    Added some preliminary rate limit checks and took out some more
    debugging info

--- src/FavoriteDeleter.java
+++ src/FavoriteDeleter.java
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @author Scotty Ward
+ * Twitter: @Scotty_ward9
+ * Email: sward2011@my.fit.edu
+ *
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,13 +15,6 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-/**
- * 
- * @author Scotty Ward
- * Twitter: @Scotty_ward9
- * Email: sward2011@my.fit.edu
- *
- */
 public class FavoriteDeleter {
 
     public static void main(String[] args) throws InterruptedException {
@@ -25,24 +26,36 @@ public class FavoriteDeleter {
         
         try {
             while (twitter.getFavorites().size() > 0) {
+                /*
+                 * TODO Add more Ratelimit checks
+                 */
                 Paging p = new Paging(1, 200);
-                System.out.println(tweets.size());
                 tweets.addAll(twitter.getFavorites(p));
-                System.out.println(tweets.size());
                 while (tweets.size() > 0) {
                     // Remove first element from list and delete
                     Status s = tweets.remove(0);
                     twitter.destroyFavorite(s.getId());
+                    /*
+                     *  TODO Add method to save Favorites from certain people
+                     */
                     //System.out.printf("%04d Deleted: %10s \n",likesRemoved, s.getId());
                     likesRemoved++;
                 }
 
                 // Sleep for 1 second.
+                System.out.println("Sleeping for a second..");
                 Thread.sleep(1000);
             }
         } catch (TwitterException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            /*
+             * Checks to see if Rate limit is exceded. If it is, sends a message
+             * to restart in ___ seconds
+             */
+            if (e.exceededRateLimitation()) {
+                System.out.printf("Rate limit excedded. Please try again in %d seconds",
+                        e.getRateLimitStatus().getSecondsUntilReset() );
+            }
+            //e.printStackTrace();
         }
         
         System.out.printf("Removed %s likes\n", likesRemoved);
